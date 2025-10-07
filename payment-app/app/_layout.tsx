aab634d942c808fee8c19f19653f3f1408080a91
«Matvei» <fore2013@mail.ru>
2025-10-07 16:30:46 +0700

    Update app configuration: add bundle identifier and status bar appearance; modify package scripts for improved build commands; enhance layout with StatusBar component

--- payment-app/app/_layout.tsx
+++ payment-app/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { styles } from "./styles";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
+      <StatusBar style="light" />
       <Stack
         initialRouteName="(tabs)"
         screenOptions={{
